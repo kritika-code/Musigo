655083888b1f29d976e0417ca68886976cbd3d00
Treeshali Sharma <sharmatreeshali@gmail.com>
2021-06-11 13:29:37 +0530

    commited

--- music genre/predictor/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Document
-
-class DocumentForm(forms.Form):
-    def is_valid(*args, **kwargs):
-        return True
-
-    file = forms.FileField(help_text='Valid .mp3 file')
--- music genre/predictor/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Document(models.Model):
-    file = models.FileField(upload_to='file/')
